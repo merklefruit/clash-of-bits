0594bdf951f1cecb958d21622c085662ae25c28c
Nicolas <nicolas@racchi.com>
2022-09-11 21:18:23 +0200

    chore: idiomatic main.rs matching

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
+use std::path::Path;
+
 mod find;
 mod load_sigs;
 mod scrape_four_bytes;
 
 fn main() {
-    // scrape_four_bytes::scrape_four_bytes().unwrap();
-    // let sigs = load_sigs::load_sigs().unwrap();
-    // let map = load_sigs::fill_hashmap(sigs);
+    if !Path::new("four_bytes.json").exists() {
+        println!("four_bytes.json not found. Running lazy scraper...");
+        match scrape_four_bytes::scrape_four_bytes() {
+            Ok(_) => println!("Scraping complete!"),
+            Err(e) => println!("Scraping failed: {}", e),
+        }
+    }
+
+    match load_sigs::load_sigs() {
+        Ok(sigs) => {
+            let map = load_sigs::fill_hashmap(sigs);
+            println!("Loaded {} signatures in the HashMap!", map.len());
+        }
+        Err(e) => println!("Error loading signatures: {}", e),
+    }
 }
